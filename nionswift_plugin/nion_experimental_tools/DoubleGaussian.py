e9bf35de1933724c1c9e745962e7bc86cc50d3be
Chris Meyer <cmeyer1969@gmail.com>
2018-10-15 10:00:35 -0700

    Consolidate access to displays to go through display items.

--- nionswift_plugin/nion_experimental_tools/DoubleGaussian.py
+++ nionswift_plugin/nion_experimental_tools/DoubleGaussian.py
@@ -76,11 +76,11 @@ class DoubleGaussianMenuItem:
 
     def menu_item_execute(self, window: API.DocumentWindow) -> None:
         document_controller = window._document_controller
-        display_specifier = document_controller.selected_display_specifier
-        data_item = document_controller.document_model.make_data_item_with_computation("nion.extension.doublegaussian", [(display_specifier.data_item, None)])
+        selected_data_item = document_controller.selected_data_item
+        data_item = document_controller.document_model.make_data_item_with_computation("nion.extension.doublegaussian", [(selected_data_item, None)])
         if data_item:
-            new_display_specifier = DataItem.DisplaySpecifier.from_data_item(data_item)
-            document_controller.display_data_item(new_display_specifier)
+            display_item = document_controller.document_model.get_display_item_for_data_item(data_item)
+            document_controller.show_display_item(display_item)
 
 
 class DoubleGaussianExtension:
