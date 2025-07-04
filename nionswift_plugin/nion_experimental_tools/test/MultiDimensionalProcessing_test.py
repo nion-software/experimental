nion-software/experimental

commit 17fc88b9d51443dfbb647ae0926fe10481d223b2

Eliminate explicit data items titles. Use automatic computation titling.

nionswift_plugin/nion_experimental_tools/MultiDimensionalProcessing.py
@@ -313,7 +313,7 @@ class MeasureShiftsMenuItemDelegate:
 
 class ApplyShifts(MultiDimensionalProcessingComputation):
     computation_id = "nion.apply_shifts"
-    label = _("Apply shifts")
+    label = _("Apply Shifts")
     inputs = {"input_data_item": {"label": _("Input data item"), "data_type": "xdata"},
               "shifts_data_item": {"label": _("Shifts data item"), "data_type": "xdata"},
               "axes_description": {"label": _("Apply shift along this axis")},
@@ -435,7 +435,6 @@ class ApplyShifts(MultiDimensionalProcessingComputation):
 
 def apply_shifts(api: Facade.API_1, window: Facade.DocumentWindow, input_di: Facade.DataItem, shifts_di: Facade.DataItem, shift_axis: str) -> Facade.DataItem:
     data_item = DataItem.DataItem(large_format=True)
-    data_item.title = f"{input_di.title} (Shifted)"
     window._document_controller.document_model.append_data_item(data_item)
     input_xdata = input_di.xdata
     assert input_xdata
@@ -533,7 +532,7 @@ class IntegrateAlongAxisMenuItemDelegate:
         integration_axes = IntegrateAlongAxis.guess_starting_axis(selected_data_item.xdata, graphic=integrate_graphic)
 
         # Make a result data item with 3 dimensions to ensure we get a large_format data item
-        result_data_item = self.__api.library.create_data_item_from_data(numpy.zeros((1,1,1)), title="Integrated {}".format(selected_data_item.title))
+        result_data_item = self.__api.library.create_data_item_from_data(numpy.zeros((1,1,1)))
 
         inputs: typing.MutableMapping[str, typing.Any]
         inputs = {"input_data_item": {"object": selected_data_item, "type": "data_source"},

nionswift_plugin/nion_experimental_tools/test/MultiDimensionalProcessing_test.py
@@ -225,8 +225,8 @@ class TestMultiDimensionalProcessing(unittest.TestCase):
             # check results
             self.assertEqual(4, len(document_model.data_items))
             self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("Aligned", aligned_haadf.title)
-            self.assertIn("Aligned", aligned_si.title)
+            self.assertIn("(Align and Integrate SI Sequence - Aligned HAADF)", aligned_haadf.title)
+            self.assertIn("(Align and Integrate SI Sequence - Aligned SI)", aligned_si.title)
 
     def test_align_si2_computation(self) -> None:
         with create_memory_profile_context() as test_context:
@@ -253,8 +253,8 @@ class TestMultiDimensionalProcessing(unittest.TestCase):
             # check results
             self.assertEqual(5, len(document_model.data_items))
             self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("Aligned and Integrated", aligned_haadf.title)
-            self.assertIn("Aligned and Integrated", aligned_si.title)
+            self.assertIn("(Align and Integrate SI Sequence - Integrated HAADF)", aligned_haadf.title)
+            self.assertIn("(Align and Integrate SI Sequence - Integrated SI)", aligned_si.title)
             self.assertIn("Measured Shifts", shifts.title)
 
     def test_align_sequence_of_multi_dim_data_computation(self) -> None:
@@ -388,7 +388,7 @@ class TestMultiDimensionalProcessing(unittest.TestCase):
             document_controller.periodic()
             self.assertEqual(3, len(document_model.data_items))
             self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("Shifted", shifted_data_item.title)
+            self.assertIn("(Apply Shifts)", shifted_data_item.title)
 
     def test_crop_multidimensional_computation(self) -> None:
         with create_memory_profile_context() as test_context:
@@ -471,7 +471,7 @@ class TestMultiDimensionalProcessing(unittest.TestCase):
             # check results
             self.assertEqual(3, len(document_model.data_items))
             self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("Joined", result_data_item.title)
+            self.assertIn("(Join Sequence)", result_data_item.title)
 
     def test_sequence_split_computation(self) -> None:
         with create_memory_profile_context() as test_context:

nionswift_plugin/nion_experimental_4dtools/DarkCorrection4D.py
@@ -36,8 +36,8 @@ class TotalBin4D:
 class DarkCorrection4D:
     label = _("4D Dark Correction")
 
-    def __init__(self, api: Facade.API_1, computation: Facade.Computation, **kwargs: typing.Any) -> None:
-        self.__api = api
+    def __init__(self, computation: Facade.Computation, **kwargs: typing.Any) -> None:
+        self.__api = getattr(computation, "api")
         self.computation = computation
 
         def create_panel_widget(ui: Facade.UserInterface, document_controller: Facade.DocumentWindow) -> Facade.ColumnWidget:
@@ -263,7 +263,6 @@ def dark_correction_4D(api: Facade.API_1, window: Facade.DocumentWindow, data_it
     crop_region._graphic.is_bounds_constrained = True
     dark_corrected_data_item = Facade.DataItem(DataItem.DataItem(large_format=True))
     dark_corrected_data_item._data_item.session_id = document_model.session_id
-    dark_corrected_data_item._data_item.title = f"{data_item._data_item.title} ({DarkCorrection4D.label})"  # explicit because auto doesn't handle multiple inputs
     document_model.append_data_item(dark_corrected_data_item._data_item)
     api.library.create_computation('nion.dark_correction_4d',
                                    inputs={'src1': data_item,
@@ -280,9 +279,9 @@ def dark_correction_4D(api: Facade.API_1, window: Facade.DocumentWindow, data_it
     return total_bin_data_item, dark_corrected_data_item
 
 
-def register_computations(api: Facade.API_1) -> None:
+def register_computations() -> None:
     Symbolic.register_computation_type('nion.total_bin_4d_SI', TotalBin4D)
-    Symbolic.register_computation_type('nion.dark_correction_4d', functools.partial(DarkCorrection4D, api))
+    Symbolic.register_computation_type('nion.dark_correction_4d', DarkCorrection4D)
 
 
 class DarkCorrection4DExtension:
@@ -295,7 +294,7 @@ class DarkCorrection4DExtension:
         api = api_broker.get_api(version="1", ui_version="1")
         # be sure to keep a reference or it will be closed immediately.
         self.__menu_item_ref = api.create_menu_item(DarkCorrection4DMenuItem(api))
-        register_computations(api)
+        register_computations()
 
     def close(self) -> None:
         self.__menu_item_ref.close()

nionswift_plugin/nion_experimental_4dtools/FramewiseDarkCorrection.py
@@ -39,8 +39,8 @@ class CalculateAverage4D:
 class FramewiseDarkCorrection:
     label = _("Framewise Dark Correction")
 
-    def __init__(self, api: Facade.API_1, computation: Facade.Computation, **kwargs: typing.Any) -> None:
-        self.__api = api
+    def __init__(self, computation: Facade.Computation, **kwargs: typing.Any) -> None:
+        self.__api = getattr(computation, "api")
         self.computation = computation
 
         def create_panel_widget(ui: Facade.UserInterface, document_controller: Facade.DocumentWindow) -> Facade.ColumnWidget:
@@ -308,7 +308,6 @@ def framewise_correction_4D(api: Facade.API_1, window: Facade.DocumentWindow, da
     dark_corrected_data_item = Facade.DataItem(DataItem.DataItem(large_format=True))
     document_model.append_data_item(dark_corrected_data_item._data_item)
     dark_corrected_data_item._data_item.session_id = document_model.session_id
-    dark_corrected_data_item._data_item.title = f"{data_item._data_item.title} ({FramewiseDarkCorrection.label})"  # explicit because auto doesn't handle multiple inputs
     api.library.create_computation('nion.framewise_dark_correction',
                                    inputs={'src1': data_item,
                                            'src2': average_data_item,
@@ -325,9 +324,9 @@ def framewise_correction_4D(api: Facade.API_1, window: Facade.DocumentWindow, da
     return average_data_item, dark_corrected_data_item
 
 
-def register_computations(api: Facade.API_1) -> None:
+def register_computations() -> None:
     Symbolic.register_computation_type('nion.calculate_4d_average', CalculateAverage4D)
-    Symbolic.register_computation_type('nion.framewise_dark_correction', functools.partial(FramewiseDarkCorrection, api))
+    Symbolic.register_computation_type('nion.framewise_dark_correction', FramewiseDarkCorrection)
 
 
 class FramewiseDarkExtension:
@@ -340,7 +339,7 @@ class FramewiseDarkExtension:
         api = api_broker.get_api(version="1", ui_version="1")
         # be sure to keep a reference or it will be closed immediately.
         self.__menu_item_ref = api.create_menu_item(FramewiseDarkMenuItem(api))
-        register_computations(api)
+        register_computations()
 
     def close(self) -> None:
         self.__menu_item_ref.close()

nionswift_plugin/nion_experimental_4dtools/Map4DRGB.py
@@ -296,46 +296,6 @@ class Map4DRGBMenuItem:
             except Exception as e:
                 self.__show_tool_tips(str(e))
 
-    def old_menu_item_execute(self, window: Facade.DocumentWindow) -> None:
-        document_controller = window._document_controller
-        selected_display_item = document_controller.selected_display_item
-        data_item = (selected_display_item.data_items[0] if
-                     selected_display_item and len(selected_display_item.data_items) > 0 else None)
-
-        if data_item:
-            api_data_item = Facade.DataItem(data_item)
-            if not (api_data_item.xdata and api_data_item.xdata.is_data_4d):
-                self.__show_tool_tips('wrong_shape')
-                return
-            map_data_item = self.__api.library.create_data_item(title='Map 4D (RGB) of ' + data_item.title)
-            # the following uses internal API and should not be used as example code.
-            computation = document_controller.document_model.create_computation()
-            computation.create_input_item("src", Symbolic.make_item(data_item))
-            computation.create_input_item("map_regions_r", Symbolic.make_item_list([]))
-            computation.create_input_item("map_regions_g", Symbolic.make_item_list([]))
-            computation.create_input_item("map_regions_b", Symbolic.make_item_list([]))
-            computation.create_variable("gamma_r", value_type="real", value=1.0)
-            computation.create_variable("gamma_g", value_type="real", value=1.0)
-            computation.create_variable("gamma_b", value_type="real", value=1.0)
-            computation.create_variable("enabled_r", value_type="boolean", value=True)
-            computation.create_variable("enabled_g", value_type="boolean", value=True)
-            computation.create_variable("enabled_b", value_type="boolean", value=True)
-            computation.processing_id = "nion.map_4d_rgb.2"
-            document_controller.document_model.set_data_item_computation(map_data_item._data_item, computation)
-            map_display_item = document_controller.document_model.get_display_item_for_data_item(map_data_item._data_item)
-            assert map_display_item
-            document_controller.show_display_item(map_display_item)
-            graphic = Graphics.PointGraphic()
-            graphic.label = "Pick"
-            graphic.role = "collection_index"
-            map_display_item.add_graphic(graphic)
-            # see note above.
-            self.__computation_data_items.update({str(data_item.uuid): 'source',
-                                                  str(map_data_item._data_item.uuid): 'map_4d'})
-            self.__show_tool_tips()
-            self.__display_item_changed_event_listener = (
-                           document_controller.focused_display_item_changed_event.listen(self.__display_item_changed))
-
 
 def map_4D_RGB(api: Facade.API_1, window: Facade.DocumentWindow, display_item: Facade.Display,
                map_regions_r: typing.Sequence[Facade.Graphic], map_regions_g: typing.Sequence[Facade.Graphic],

nionswift_plugin/nion_experimental_4dtools/test/Computations4D_test.py
@@ -80,7 +80,7 @@ class TestComputations4D(unittest.TestCase):
             display_item = document_model.get_display_item_for_data_item(data_item)
             display_panel.set_display_panel_display_item(display_item)
             api = Facade.get_api("~1.0", "~1.0")
-            DarkCorrection4D.register_computations(api)
+            DarkCorrection4D.register_computations()
             bin_data_item, corrected_data_item = DarkCorrection4D.dark_correction_4D(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), 'auto', True, None)
             document_model.recompute_all()
             document_controller.periodic()
@@ -101,7 +101,7 @@ class TestComputations4D(unittest.TestCase):
             display_item = document_model.get_display_item_for_data_item(data_item)
             display_panel.set_display_panel_display_item(display_item)
             api = Facade.get_api("~1.0", "~1.0")
-            FramewiseDarkCorrection.register_computations(api)
+            FramewiseDarkCorrection.register_computations()
             average_data_item, corrected_data_item = FramewiseDarkCorrection.framewise_correction_4D(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), 'auto', True, None)
             document_model.recompute_all()
             document_controller.periodic()

nionswift_plugin/nion_experimental_tools/AffineTransformImage.py
@@ -118,7 +118,7 @@ class AffineTransformMenuItem:
 
         assert data_item.xdata
 
-        result_data_item = self.__api.library.create_data_item_from_data_and_metadata(DataAndMetadata.new_data_and_metadata(numpy.zeros_like(data_item.data), data_descriptor=data_item.xdata.data_descriptor), title="Affine Transformed {}".format(data_item.title))
+        result_data_item = self.__api.library.create_data_item_from_data_and_metadata(DataAndMetadata.new_data_and_metadata(numpy.zeros_like(data_item.data), data_descriptor=data_item.xdata.data_descriptor))
         self.__api.library.create_computation("nion.affine_transform_image",
                                               inputs={"src_data_item": data_item, "vector_a": [vector_a], "vector_b": [vector_b]},
                                               outputs={"target": result_data_item})
@@ -175,7 +175,7 @@ class CopyAffineTransformMenuItem:
             typing.cast(Graphics.LineGraphic, vector_b._graphic).end_arrow_enabled = True
 
             assert data_item_.xdata
-            result_data_item = self.__api.library.create_data_item_from_data_and_metadata(DataAndMetadata.new_data_and_metadata(numpy.zeros_like(data_item_.data), data_descriptor=data_item_.xdata.data_descriptor), title="Affine Transformed {}".format(data_item_.title))
+            result_data_item = self.__api.library.create_data_item_from_data_and_metadata(DataAndMetadata.new_data_and_metadata(numpy.zeros_like(data_item_.data), data_descriptor=data_item_.xdata.data_descriptor))
             self.__api.library.create_computation("nion.affine_transform_image",
                                                   inputs={"src_data_item": data_item_, "vector_a": [vector_a], "vector_b": [vector_b]},
                                                   outputs={"target": result_data_item})

nionswift_plugin/nion_experimental_tools/AlignMultiSI.py
@@ -15,6 +15,12 @@ _ = gettext.gettext
 
 
 class AlignMultiSI(Symbolic.ComputationHandlerLike):
+    label = _("Align and Integrate SI Sequence")
+    outputs = {"shifts": {"label": _("Measured Shifts")},
+               "aligned_haadf": {"label": _("Aligned HAADF")},
+               "aligned_si": {"label": _("Aligned SI")},
+               }
+
     def __init__(self, computation: Facade.Computation, **kwargs: typing.Any) -> None:
         self.computation = computation
         self.progress_updated_event = Event.Event()
@@ -135,7 +141,7 @@ class AlignMultiSI(Symbolic.ComputationHandlerLike):
 
 class AlignMultiSI2(Symbolic.ComputationHandlerLike):
     computation_id = "eels.align_multi_si2"
-    label = _("Align and integrate SI sequence")
+    label = _("Align and Integrate SI Sequence")
     inputs = {"haadf_data_item": {"label": _("HHADF data item"), "data_type": "xdata"},
               "si_data_item": {"label": _("SI data item"), "data_type": "xdata"},
               "reference_index": {"label": _("Reference index for shifts")},
@@ -143,7 +149,7 @@ class AlignMultiSI2(Symbolic.ComputationHandlerLike):
               "max_shift": {"label": _("Max shift between consecutive frames (in pixels, <= 0 to disable)")},
               "bounds_graphic": {"label": _("Shift bounds")},
               }
-    outputs = {"shifts": {"label": _("Shifts")},
+    outputs = {"shifts": {"label": _("Measured Shifts")},
                "integrated_haadf": {"label": _("Integrated HAADF")},
                "integrated_si": {"label": _("Integrated SI")},
                }
@@ -208,8 +214,8 @@ def menu_item_align_multi_si(api: API, window: API.DocumentWindow) -> None:
     align_multi_si(api, window, haadf_sequence_data_item, align_region, si_sequence_data_item, align_index)
 
 def align_multi_si(api: API, window: API.DocumentWindow, haadf_sequence_data_item: Facade.DataItem, bounds_graphic: Facade.Graphic | None, si_sequence_data_item: Facade.DataItem, align_index: int) -> tuple[Facade.DataItem, Facade.DataItem]:
-    aligned_haadf = api.library.create_data_item_from_data(numpy.zeros((1,1,1)), title=f"{haadf_sequence_data_item.title} (Aligned)")
-    aligned_si = api.library.create_data_item_from_data(numpy.zeros((1,1,1)), title=f"{si_sequence_data_item.title} (Aligned)")
+    aligned_haadf = api.library.create_data_item_from_data(numpy.zeros((1,1,1)))
+    aligned_si = api.library.create_data_item_from_data(numpy.zeros((1,1,1)))
 
     inputs = {"si_sequence_data_item": si_sequence_data_item,
               "haadf_sequence_data_item": haadf_sequence_data_item,
@@ -229,10 +235,10 @@ def align_multi_si(api: API, window: API.DocumentWindow, haadf_sequence_data_ite
 
 
 def align_multi_si2(api: API, window: API.DocumentWindow, haadf_sequence_data_item: Facade.DataItem, bounds_graphic: Facade.Graphic | None, si_sequence_data_item: Facade.DataItem) -> tuple[Facade.DataItem, Facade.DataItem, Facade.DataItem]:
-    aligned_haadf = api.library.create_data_item(title=f"{haadf_sequence_data_item.title} (Aligned and Integrated)")
+    aligned_haadf = api.library.create_data_item()
     # Make a result data item with 3 dimensions to ensure we get a large_format data item
-    aligned_si = api.library.create_data_item_from_data(numpy.zeros((1,1,1)), title=f"{si_sequence_data_item.title} (Aligned and Integrated)")
-    shifts = api.library.create_data_item_from_data(numpy.zeros((2, 2)), title=f"{haadf_sequence_data_item.title} (Measured Shifts)")
+    aligned_si = api.library.create_data_item_from_data(numpy.zeros((1,1,1)))
+    shifts = api.library.create_data_item_from_data(numpy.zeros((2, 2)))
 
     haadf_xdata = haadf_sequence_data_item.xdata
     assert haadf_xdata

commit 45b4bc8433f86e95ba4c3f93fd9622a55164424a

Fix regression introduced in 17fc88b9d51443dfbb647ae0926fe10481d223b2 (breaks apply shifts computation for certain data shapes). Add test.

nionswift_plugin/nion_experimental_tools/MultiDimensionalProcessing.py
@@ -381,8 +381,13 @@ class ApplyShifts(MultiDimensionalProcessingComputation):
         assert input_xdata is not None
         assert shifts_data_item.xdata is not None
         assert shifts_data_item.data is not None
-        if shifts_data_item.xdata.data_descriptor.datum_dimension_count == 1:
-            shifts = shifts_data_item.data.T
+        shifts_shape = shifts_data_item.xdata.data_shape
+        # Handle the special case of shifts created by "AlignImageSequence" here: This computation calculates the shifts
+        # for a 1D collection or a sequence of 2D data and transposes the result so that Swift can display it as a
+        # line plot. Try to detect that case and transpose back.
+        if len(shifts_shape) == 2 and shifts_shape[0] == 2 and shifts_shape[-1] == input_xdata.data_shape[0]:
+            # HDF5 datasets don't implement .T, so convert to real numpy array first
+            shifts = numpy.asarray(shifts_data_item.data).T
         else:
             shifts  = shifts_data_item.data
         split_description = axes_description.split("-")
@@ -495,7 +500,11 @@ class ApplyShiftsMenuItemDelegate:
         else:
             raise ValueError(error_msg)
 
-        if shifts_display.display_type == "line_plot":
+        # Handle the special case of shifts created by "AlignImageSequence" here: This computation calculates the shifts
+        # for a 1D collection or a sequence of 2D data and transposes the result so that Swift can display it as a
+        # line plot. Try to detect that case and transpose back for guessing the starting axis.
+        shifts_shape = shifts_di.xdata.data_shape
+        if shifts_display.display_type == "line_plot" and len(shifts_shape) == 2 and shifts_shape[0] == 2 and shifts_shape[-1] == input_di.xdata.data_shape[0]:
             shifts_xdata = Core.function_transpose_flip(shifts_di.xdata, transpose=True, flip_v=False, flip_h=False)
         else:
             shifts_xdata = shifts_di.xdata

nionswift_plugin/nion_experimental_tools/test/MultiDimensionalProcessing_test.py
@@ -368,30 +368,57 @@ class TestMultiDimensionalProcessing(unittest.TestCase):
             self.assertIn("iDPC", idpc_data_item.title)
 
     def test_measure_shifts_computation_and_apply_shifts_computation(self) -> None:
-        with create_memory_profile_context() as test_context:
-            document_controller = test_context.create_document_controller_with_application()
-            document_model = document_controller.document_model
-            api = Facade.get_api("~1.0", "~1.0")
-            # setup
-            xdata = DataAndMetadata.new_data_and_metadata(numpy.random.randn(8,8,8), data_descriptor=DataAndMetadata.DataDescriptor(True, 0, 2))
-            data_item = DataItem.new_data_item(xdata)
-            data_item.title = "HAADF"
-            document_model.append_data_item(data_item)
-            # make computation and execute
-            shifts_data_item = MultiDimensionalProcessing.measure_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), None, "data")
-            document_model.recompute_all()
-            document_controller.periodic()
-            # check results
-            self.assertEqual(2, len(document_model.data_items))
-            self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("Measure Shifts", shifts_data_item.title)
-            # make computation and execute
-            shifted_data_item = MultiDimensionalProcessing.apply_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), shifts_data_item, "data")
-            document_model.recompute_all()
-            document_controller.periodic()
-            self.assertEqual(3, len(document_model.data_items))
-            self.assertFalse(any(computation.error_text for computation in document_model.computations))
-            self.assertIn("(Apply Shifts)", shifted_data_item.title)
+        with self.subTest(msg="Test for a sequence of 2D data. Measure shifts in data axis."):
+            with create_memory_profile_context() as test_context:
+                document_controller = test_context.create_document_controller_with_application()
+                document_model = document_controller.document_model
+                api = Facade.get_api("~1.0", "~1.0")
+                # setup
+                xdata = DataAndMetadata.new_data_and_metadata(numpy.random.randn(8,8,8), data_descriptor=DataAndMetadata.DataDescriptor(True, 0, 2))
+                data_item = DataItem.new_data_item(xdata)
+                data_item.title = "HAADF"
+                document_model.append_data_item(data_item)
+                # make computation and execute
+                shifts_data_item = MultiDimensionalProcessing.measure_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), None, "data")
+                document_model.recompute_all()
+                document_controller.periodic()
+                # check results
+                self.assertEqual(2, len(document_model.data_items))
+                self.assertFalse(any(computation.error_text for computation in document_model.computations))
+                self.assertIn("Measure Shifts", shifts_data_item.title)
+                # make computation and execute
+                shifted_data_item = MultiDimensionalProcessing.apply_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), shifts_data_item, "data")
+                document_model.recompute_all()
+                document_controller.periodic()
+                self.assertEqual(3, len(document_model.data_items))
+                self.assertFalse(any(computation.error_text for computation in document_model.computations))
+                self.assertIn("(Apply Shifts)", shifted_data_item.title)
+
+        with self.subTest(msg="Test for a 2D collection of 2D data. Measure shifts in data axis."):
+            with create_memory_profile_context() as test_context:
+                document_controller = test_context.create_document_controller_with_application()
+                document_model = document_controller.document_model
+                api = Facade.get_api("~1.0", "~1.0")
+                # setup
+                xdata = DataAndMetadata.new_data_and_metadata(numpy.random.randn(7,8,9,10), data_descriptor=DataAndMetadata.DataDescriptor(False, 2, 2))
+                data_item = DataItem.new_data_item(xdata)
+                data_item.title = "HAADF"
+                document_model.append_data_item(data_item)
+                # make computation and execute
+                shifts_data_item = MultiDimensionalProcessing.measure_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), None, "data")
+                document_model.recompute_all()
+                document_controller.periodic()
+                # check results
+                self.assertEqual(2, len(document_model.data_items))
+                self.assertFalse(any(computation.error_text for computation in document_model.computations))
+                self.assertIn("Measure Shifts", shifts_data_item.title)
+                # make computation and execute
+                shifted_data_item = MultiDimensionalProcessing.apply_shifts(api, Facade.DocumentWindow(document_controller), Facade.DataItem(data_item), shifts_data_item, "data")
+                document_model.recompute_all()
+                document_controller.periodic()
+                self.assertEqual(3, len(document_model.data_items))
+                self.assertFalse(any(computation.error_text for computation in document_model.computations))
+                self.assertIn("(Apply Shifts)", shifted_data_item.title)
 
     def test_crop_multidimensional_computation(self) -> None:
         with create_memory_profile_context() as test_context: